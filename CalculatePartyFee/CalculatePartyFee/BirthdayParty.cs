3ff915260dd7d300cfe7c202a7db44be9456797a
anhphuong0204 <phuong.nguyen02042000@hcmut.edu.vn>
2020-04-09 18:06:35 +0700

    Modify and add birthdayParty class

---
+++ CalculatePartyFee/CalculatePartyFee/BirthdayParty.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatePartyFee
+{
+    class BirthdayParty
+    {
+        private int numOfPeople;
+        private bool fancyDecorations;
+        private string cakeWriting;
+        private const int costOfFoodPerPerson = 25;
+        
+        public int NumOfPeople { get { return numOfPeople; } set { numOfPeople = value; } }
+        public bool FancyDecorations { get { return fancyDecorations; } set { fancyDecorations = value; } }
+        public string CakeWriting { 
+            get { return cakeWriting; } 
+            set {
+                cakeWriting = value;
+            }
+        }
+        private int ActualLength { 
+            get {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();
+                return CakeWriting.Length;
+            }
+        }
+        public bool CakeWritingTooLong
+        {
+            get {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return true;
+                return false;
+            }
+            
+        }
+        public BirthdayParty(int numOfPeople, bool fancyDecorations, string cakeWriting)
+        {
+            this.numOfPeople = numOfPeople;
+            this.fancyDecorations = fancyDecorations;
+            this.cakeWriting = cakeWriting;
+        }
+        private int CakeSize()
+        {
+            if (numOfPeople <= 4)
+                return 8;
+            return 16;
+        }
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+                return 16;
+            return 40;
+        }
+
+        private decimal CalcDecorationCost()
+        {
+            if (fancyDecorations)
+                return numOfPeople * 15M + 50M;
+            return numOfPeople * 7.5M + 30M;
+        }
+        public decimal Cost { 
+            get {
+                return numOfPeople * costOfFoodPerPerson + ((CakeSize() == 8) ? 40M + ActualLength * .25M : 75M + ActualLength * .25M) + CalcDecorationCost();
+                                                                                // Small cake               // Big cake
+            }
+        }
+    }
+}
--- CalculatePartyFee/CalculatePartyFee/Form1.cs
+++ CalculatePartyFee/CalculatePartyFee/Form1.cs
@@ -13,34 +13,66 @@ namespace CalculatePartyFee
     public partial class Form1 : Form
     {
         DinnerParty dinnerParty;
+        BirthdayParty birthdayParty;
         public Form1()
         {
             InitializeComponent();
+
             dinnerParty = new DinnerParty((int)numericUpDown1.Value, decorationCheckbox.Checked, beverageCheckbox.Checked);
-            DisplayCost();
+            DisplayDinnerCost();
+
+            birthdayParty = new BirthdayParty((int)numericUpDown2.Value, fancyCheckbox.Checked, cakeWriting.Text);
+            DisplayBirthdayCost();
         }
-        private void DisplayCost()
+        private void DisplayDinnerCost()
         {
             decimal cost = dinnerParty.Cost;
             costLabel.Text = cost.ToString("c");
         }
+        private void DisplayBirthdayCost()
+        {
+            tooLongLabel.Visible = birthdayParty.CakeWritingTooLong;
+            decimal cost = birthdayParty.Cost;
+            costBirthdayParty.Text = cost.ToString("c");
+        }
 
+        // Dinner Party Control
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
             dinnerParty.NumOfPeople = (int)numericUpDown1.Value;
-            DisplayCost();
+            DisplayDinnerCost();
         }
 
         private void decorationCheckbox_CheckedChanged(object sender, EventArgs e)
         {
             dinnerParty.FancyDecoration = decorationCheckbox.Checked;
-            DisplayCost();
+            DisplayDinnerCost();
         }
 
         private void beverageCheckbox_CheckedChanged(object sender, EventArgs e)
         {
             dinnerParty.HealthyOption = beverageCheckbox.Checked;
-            DisplayCost();
+            DisplayDinnerCost();
+        }
+
+
+        // Birthday Party Control
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            birthdayParty.NumOfPeople = (int)numericUpDown2.Value;
+            DisplayBirthdayCost();
+        }
+
+        private void fancyCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            birthdayParty.FancyDecorations = fancyCheckbox.Checked;
+            DisplayBirthdayCost();
+        }
+
+        private void cakeWriting_TextChanged(object sender, EventArgs e)
+        {
+            birthdayParty.CakeWriting = cakeWriting.Text;
+            DisplayBirthdayCost();
         }
     }
 }
