f85c385de94fefc7e93295c6f9001c9eadaf88af
anhphuong0204 <phuong.nguyen02042000@hcmut.edu.vn>
2020-05-05 22:42:54 +0700

    Add graphics to the game

--- ExploringDungeons/ExploringDungeons/Form1.cs
+++ ExploringDungeons/ExploringDungeons/Form1.cs
@@ -12,9 +12,25 @@ namespace ExploringDungeons
 {
     public partial class Form1 : Form
     {
+        private Game game;
+        private Random random = new Random();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender,
+        EventArgs e)
+        {
+            game = new Game(new Rectangle(78, 57, 420, 155));
+            game.NewLevel(random);
+            UpdateCharacters();
+        }
+
+        private void UpdateCharacters()
+        {
+
         }
     }
 }
---
+++ ExploringDungeons/ExploringDungeons/Potion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploringDungeons
+{
+    class BluePotion : Weapon, IPotion
+    {
+        public BluePotion(Game game, Point point) : base(game, point) {
+            used = false;
+        }
+        public override string Name { get { return "Blue Potion"; } }
+
+        private bool used;
+        public bool Used { get { return used; } }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            // increase player health up to 5p
+        }
+    }
+
+    class RedPotion : Weapon, IPotion
+    {
+        public RedPotion(Game game, Point point) : base(game, point)
+        {
+            used = false;
+        }
+        public override string Name { get { return "Red Potion"; } }
+
+        private bool used;
+        public bool Used { get { return used; } }
+
+        public override void Attack(Direction direction, Random random)
+        {
+            // increase player health up to 10p
+        }
+    }
+}
